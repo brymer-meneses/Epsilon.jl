d2de14a206eaff8c9d2c54af1e623d79d3ad5b41
Brymer-Meneses <meneses.brymer@gmail.com>
2021-03-04 00:49:30 +0800

    Implement Tensor Ops

--- src/Epsilon.jl
+++ src/Epsilon.jl
@@ -1,5 +1,45 @@
 module Epsilon
 
-greet() = print("Hello World!")
+# imports 
+using LinearAlgebra
 
+struct Tensor
+    data::Matrix{Float64}
+    requires_grad::Bool
+    dependency::Array
+
+    shape::Tuple{Int64, Int64}
+    
+    function Tensor(data, requires_grad=false, dependency=Array[]) 
+        shape = size(data)
+
+        return new(data, requires_grad, dependency, shape)
+    end
+
+    function Base.:show(io::IO, t1::Tensor)
+        display(t1.data)
+    end
+
+    function Base.:-(t1::Tensor, t2::Tensor)
+        result = t1.data - t2.data
+        return result
+    end
+
+    function Base.:+(t1::Tensor, t2::Tensor)
+        result = t1.data + t2.data
+        return result
+    end
+
+    function Base.:*(t1::Tensor, t2::Tensor)
+        result = t1.data * 
+        return result
+    end
+end
+
+
+
+function greet()
+    println("hello from Epsilon")
+end
+    
 end # module
