ff53d144b3930ae7d64b6349419797f7da3daa0f
Brymer-Meneses <meneses.brymer@gmail.com>
2021-03-04 15:54:59 +0800

    Refactor

--- src/Epsilon.jl
+++ src/Epsilon.jl
@@ -3,39 +3,85 @@ module Epsilon
 # imports 
 using LinearAlgebra
 
-struct Tensor
-    data::Matrix{Float64}
-    requires_grad::Bool
-    dependency::Array
+struct Dependency 
+    t::Tensor 
+    grad_fn
 
-    shape::Tuple{Int64, Int64}
+    function Dependency(t, grad_fn)
+        return new(t, grad_fn)
+    end 
+
+end
+
+struct Tensor <: AbstractArray{Float64, Float64}
     
-    function Tensor(data, requires_grad=false, dependency=Array[]) 
+    data::Array{Float64}
+    requires_grad::Bool
+    depends_on::Array{Dependency}
+
+    shape
+    
+    function Tensor(data, requires_grad=false, depends_on =Array[]) 
         shape = size(data)
 
-        return new(data, requires_grad, dependency, shape)
+        # Makes it possible to use Tensor(1)
+        if shape == ()
+            data = [data]
+        end
+
+        return new(data, requires_grad, depends_on, shape)
+    end
+
+    function Base.:transpose(t1::Tensor)
+        return Tensor(transpose(t1.data), t1.requires_grad, t1.depends_on)
     end
 
     function Base.:show(io::IO, t1::Tensor)
         display(t1.data)
     end
 
-    function Base.:-(t1::Tensor, t2::Tensor)
-        result = t1.data - t2.data
-        return result
-    end
-
     function Base.:+(t1::Tensor, t2::Tensor)
-        result = t1.data + t2.data
-        return result
+        return add(t1, t2)
     end
 
     function Base.:*(t1::Tensor, t2::Tensor)
-        result = t1.data * 
-        return result
+        return mul(t1, t2)
     end
+
+    function Base.:sum(t1::Tensor)
+        return tensor_sum(t1)
+    end
+
 end
 
+function tensor_sum(t::Tensor)
+    result = sum(t.data)
+    requires_grad = t.requires_grad
+
+    if requires_grad
+        function grad_fn(grad)
+            
+            grad * ones(t.shape[1], t.shape[2])
+            return grad
+        end
+
+        depends_on = Dependency(t, grad_fn)
+    else 
+        depends_on = Nothing
+
+    return Tensor(data, requires_grad, depends_on)
+end
+
+
+function add(t1::Tensor, t2::Tensor)
+    result = t1.data + t2.data
+    return result
+end
+
+function mul(t1::Tensor, t2::Tensor)
+    result = t1.data * t2.data
+    return result
+end
 
 
 function greet()
